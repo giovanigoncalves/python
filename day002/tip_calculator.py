de7f0776170e1ab66ecaafb226dcfdcd547d5ce8
Giovani G. Ribamar <42078063+giovanigoncalves@users.noreply.github.com>
2022-06-25 14:58:34 -0300

    Update tip_calculator.py

--- day002/tip_calculator.py
+++ day002/tip_calculator.py
@@ -7,14 +7,15 @@
 
 #Write your code below this line 👇
 
-print("\n\nWelcome to the tip calculator.")
-total_bill = float(input("What was the total bill? $" ))
-perc_tip = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+print("\nWelcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $" )) 
+perc_tip = float(input("What percentage tip would you like to give? 10, 12, or 15? ")) # 
 n_people = int(input("How many people to split the bill? "))
 
-total_bill += total_bill * (perc_tip / 100)
-individual_bill = total_bill / n_people
-individual_bill = round(individual_bill, 2)
+total_bill += total_bill * (perc_tip / 100) # Bill with the tip chosen
+individual_bill = total_bill / n_people # Amount for each one
 
-print(f"Each person should pay: ${individual_bill}\n\n")
+individual_bill = round(individual_bill, 2) # Trancating the result with 2 decimal digits
+
+print(f"Each person should pay: ${individual_bill}\n") # Printing the result
 
