a82001f72527f858a87451cc340fb457147eb96f
Giovani G. Ribamar <42078063+giovanigoncalves@users.noreply.github.com>
2022-06-30 18:20:13 -0300

    hangman_game.py added

---
+++ day007/hangman_game.py
@@ -0,0 +1,40 @@
+from random import choice
+from hangman_words import word_list
+from hangman_art import stages, logo
+
+chosen_word = choice(word_list)
+
+print(f"{logo}")
+
+display = []
+for letter in chosen_word:
+    display += "_"
+
+lives = 6
+end_game = False
+while not end_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in chosen_word:
+        for pos in range(len(chosen_word)):
+            if chosen_word[pos] == guess:
+                display[pos] = guess
+    else:
+        print(f'There is no "{guess}" in the word.')
+        lives -= 1
+    
+    if lives == 0:
+        print("You lose!")
+        end_game = True
+  
+    if "_" not in display:
+        end_game = True
+        print("You've won!")
+
+    print(f"{' '.join(display)}")
+
+    print(stages[lives])
+    
+    print(f'The correct word is "{chosen_word}"!')
+
+     
