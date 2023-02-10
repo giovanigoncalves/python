e63b1ea3c9fddfc7611cd205780272d28a580b13
Giovani G. Ribamar <42078063+giovanigoncalves@users.noreply.github.com>
2023-02-09 23:15:45 -0300

    day49 update
    Update of the script that looks for "Data Science" jobs in Brazil as contract type, saves the job and create a txt file with job name, location and company name.

--- day049/main.py
+++ day049/main.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from time import sleep
+from time import sleep 
+from datetime import date
+import pandas as pd
 
+data = pd.read_csv(r'C:\Users\T-Gamer\Documents\data\data.txt')
 
 service = Service("/home/giovani/Softwares/chromedriver_linux64/chromedriver")
 driver = webdriver.Chrome(service=service)
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3318540929&f_AL=true&f_C=%5B%5D&f_E=%5B%5D&f_EA=%5B%5D&f_F=%5B%5D&f_I=%5B%5D&f_JC=%5B%5D&f_JIYN=%5B%5D&f_JT=%5B%5D&f_PP=%5B%5D&f_T=%5B%5D&f_WT=%5B%5D&keywords=python%20developer&sortBy=R")
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3477237770&f_JT=C&keywords=data%20science")
 driver.maximize_window()
 
 sleep(3)
@@ -18,25 +21,56 @@ sleep(1)
 
 
 email = driver.find_element(By.ID, "username")
-email.send_keys("put_your_email_here")
+email.send_keys(data.username)
 password = driver.find_element(By.ID, "password")
-password.send_keys("put your email password here")
+password.send_keys(data.password)
 sign_in = driver.find_element(By.CSS_SELECTOR, ".login__form_action_container  .btn__primary--large")
 sign_in.click()
-sleep(3)
+sleep(12)
 
-# hide_message = driver.find_element(By.CLASS_NAME, "mercado-match")
-# hide_message.click()
 
-job_list = driver.find_elements(By.CSS_SELECTOR, ".scaffold-layout__list-container li")
-# print(len(job_list))
-title = []
+job_list = driver.find_elements(By.CSS_SELECTOR, "ul.scaffold-layout__list-container li.ember-view")
+
+
+title_job = []
+company_name = []
+job_location = []
 for job in job_list:
-    sleep(1.)
-    if len(job.get_attribute("class")) != 0 and "ember-view   jobs-search-results__list-item occludable-update p0 relative scaffold-layout__list-item" in job.get_attribute("class"):
-        job.click()
-        title_job = driver.find_element(By.CSS_SELECTOR, '#ember130 h2').text
-        title.append(title_job)
-        save = job.find_element(By.CSS_SELECTOR, 'button span')
-        save.click()
-        sleep(1.)
+    job.click()
+    job.click()
+    title = driver.find_element(By.CSS_SELECTOR, 'h2.t-24').text
+    title_job.append(title)
+    print(title)
+    company = driver.find_element(By.CSS_SELECTOR, 'span.jobs-unified-top-card__company-name a').text
+    company_name.append(company)
+    location = driver.find_element(By.CSS_SELECTOR, 'span.jobs-unified-top-card__bullet').text
+    job_location.append(location)
+    print(location)
+    
+    text_saved = driver.find_element(By.CSS_SELECTOR, 'div.mt5 button.jobs-save-button span').text
+    print(text_saved)
+    sleep(2.5)
+    if text_saved == 'Salvar':
+        save_button = driver.find_element(By.CSS_SELECTOR, 'div.mt5 button.jobs-save-button span')
+        save_button.click()
+        print("Saved!")
+    else:
+        print(f"{title} already saved!")
+        
+    sleep(3)
+
+
+# for job in title_job:
+#     print(job)
+    
+# for company in company_name:
+#     print(company)
+    
+# for location in job_location:
+#     print(location)
+    
+with open(f"./jobs_list_{date.today()}.txt", "w", encoding="utf-8") as job_list_today:
+    job_list_today.writelines(f"job;location;company\n")
+    for i in range(len(title_job)):
+        job_list_today.writelines(f"{title_job[i]};{job_location[i]};{company_name[i]}\n")
+    
---
+++ teste.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+df = pd.read_csv("jobs_list_2023-02-09.txt", sep=";")
+
+print(df)
